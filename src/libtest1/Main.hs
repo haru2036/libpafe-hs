228df2e74c0a6c8ab87e4be5ea9f3a0d6e0f7409
haru2036 <haru2036@gmail.com>
2015-05-09 01:00:39 +0900

    fixed libtest1

--- src/libtest1/Main.hs
+++ src/libtest1/Main.hs
@@ -2,6 +2,7 @@ import Bindings.Libpafe.Pasori
 import Bindings.Libpafe.Types
 import Bindings.Libpafe.Felica
 import Foreign.Ptr
+import Foreign.Storable
 
 main :: IO()
 main = do
@@ -9,6 +10,7 @@ main = do
   case maybePasori of
     Just pasori -> do
       felica <- felica_polling pasori 0xffff 0 0 
-      print felica
+      print "IDm is:"
+      print =<< peek felica
       pasoriClose pasori
     Nothing -> print "pasori is nothing"
