332478a66dc8cfef29816b29629b406c4b2e3d09
haru2036 <haru2036@gmail.com>
2015-05-13 04:23:01 +0900

    fixed a executable for testing

--- src/libtest1/Main.hs
+++ src/libtest1/Main.hs
@@ -9,12 +9,16 @@ main = do
   maybePasori <- pasoriPrepare
   case maybePasori of
     Just pasori -> do
-      felicaPtr <- felica_polling pasori 0xffff 0 0 
+      felicaPtr <- felica_polling pasori 0xfe00 0 0 
       felica <- peek felicaPtr
       print "IDm is:"
       print felica
       print "PMm is:"
       print $ pmm felica
+      -- maybeBlockWord <- mapM (felicaReadSingle felicaPtr 0 0x1A8B) [0..10]
+      maybeBlockWord <- felicaRead felicaPtr 1 $ FelicaBlockInfo 0x1A8B 0 0 
+      print maybeBlockWord
+
       {-
       print "Area Number is:"
       print $ areaNum felica
